232ad460342bdac3f287426911ebc67404fcda11
linpicio <linpicio@gmail.com>
2026-04-17 15:41:16 +0800

    fix(config): 更新任务起始时间并优化视频生成提示词

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const config = {
   LOG_FILE: "./logs/app.log",
   USER_DATA_DIR: "./.user_data",
   EXPLORE_URL: "https://tongyi.aliyun.com/wan/explore",
-  START_TIME: new Date("2026-04-17 00:00:00 GMT+0800").getTime(),
+  START_TIME: new Date("2036-04-17 00:00:00 GMT+0800").getTime(),
   SUBMIT_PAYLOAD: {
     deductMode: "relax_mode",
     taskType: "image_to_video",
@@ -21,13 +21,7 @@ export const config = {
       duration: 15,
       assistInfo: "{}",
       prompt:
-        "镜头固定，画面中皇帝畅快地从金身武僧口中吸取内力。武僧身体微微颤抖，手下意识抓紧。随着内力被逐渐吸干，武僧的身体变得干瘪脱水萎缩。最后，皇帝畅快地呼出一口气，与武僧嘴对嘴，武僧瘫软在床上颤抖无力地发出“唔嗯”的声音。整体画面采用暗黑武侠风格，近景特写，强调内力流动和人物表情。带有悲怆和危机的背景音乐，以及微弱的内力流动的声音。",
-      promptMeta: {
-        originPrompt:
-          "镜头固定，画面中皇帝畅快地从金身武僧口中吸取内力。武僧身体微微颤抖，手下意识抓紧。随着内力被逐渐吸干，武僧的身体变得干瘪脱水萎缩。最后，皇帝畅快地呼出一口气，与武僧嘴对嘴，武僧瘫软在床上颤抖无力地发出“唔嗯”的声音。整体画面采用暗黑武侠风格，近景特写，强调内力流动和人物表情。带有悲怆和危机的背景音乐，以及微弱的内力流动的声音。",
-        orderedKeys: [],
-        refs: {},
-      },
+        "皇帝畅快地从完全昏迷的金身武僧口中吸取内力。武僧身体微微颤抖。随着内力被逐渐吸干，武僧的身体变得干瘪脱水萎缩。皇帝畅快地呼出一口气，与武僧嘴对嘴亲一口，然后说：“你的内力终于是我的了”，最后是皇帝得意而略带阴险的正脸特写。整体画面采用暗黑武侠风格，近景特写，强调内力流动和人物表情。带有悲怆和危机的背景音乐，以及微弱的内力流动的声音。",
       generationMode: "plain",
       baseImage:
         "https://cdn.wanx.aliyuncs.com/upload/5231657482591727577/unknown/83d2bceb1a904a19bff840f0c6a9acfd1776277295916.jpg?x-oss-process=image/resize,w_500/format,webp",
